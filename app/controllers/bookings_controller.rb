6af029742c0d809534351f8a260c52a1eb02af33
lakshit gupta <lakshitgupta218@gmail.com>
2024-07-02 19:40:22 +0530

    bookings are done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,6 +10,18 @@ class BookingsController < ApplicationController
   def create
     @booking = @seat.bookings.new(booking_params)
     @booking.user = current_user
+    @booking_date = Date.new(params[:booking]["booking_date(1i)"].to_i, 
+                          params[:booking]["booking_date(2i)"].to_i, 
+                          params[:booking]["booking_date(3i)"].to_i)
+
+    # Check if the bus departure time has passed and booking date is in the past
+    # debugger
+    if @bus.departure_time.strftime("%H:%M:%S") < Time.now.strftime("%H:%M:%S") && @booking_date < Date.today
+      flash.now[:alert] = "Booking cannot be made after the departure time of the bus."
+      render :new
+      return
+    end
+
     if @booking.save
       redirect_to bus_seats_path(@bus), notice: 'Seat was successfully booked.'
     else
@@ -29,5 +41,13 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:booking_date)
+  end
+
+  def booking_date_in_past?
+    # Convert booking_date to a Date object and check if it's in the past
+    booking_date = Date.parse(params[:booking][:booking_date])
+    booking_date < Date.today
+  rescue ArgumentError
+    false  # Handle if booking_date cannot be parsed correctly
   end
 end
